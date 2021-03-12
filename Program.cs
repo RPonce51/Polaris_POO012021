f4d82d108447445f031e4ed5d020f32b427a8120
RPonce51 <00143220@uca.edu.sv>
2021-03-11 18:28:01 -0600

    IntroGit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IntroGit
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            Console.WriteLine("Ingrese el valor de a:");
+            a = Int32.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Ingrese el valor de b:");
+            b = Int32.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"Sumando {a} + {b} resulta {Addnumbers(a, b)}");
+        }
+
+        public static int Addnumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
